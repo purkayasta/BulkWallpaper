684273ef19188c4128ec6c50d83ec2bfc536eae3
purkayasta <pritompurkayasta@gmail.com>
2021-04-25 22:31:47 +0600

    Initial release

---
+++ BulkImageDownloader.Cli/Client.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BulkImageDownloader.Cli
+{
+    class Client
+    {
+        internal static async Task CallClient(string url, string localPathWithImageName)
+        {
+            using var httpClient = new HttpClient();
+            using var response = await httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+            using var fileStream = await response.Content.ReadAsStreamAsync();
+            using var streamWriter = File.Open(localPathWithImageName, FileMode.Create);
+            await fileStream.CopyToAsync(streamWriter);
+        }
+    }
+}
---
+++ BulkImageDownloader.Cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BulkImageDownloader.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var serviceCollection = new ServiceCollection();
+
+        }
+    }
+}
