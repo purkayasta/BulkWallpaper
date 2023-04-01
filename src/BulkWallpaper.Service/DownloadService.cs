1db93fbf83d90bf73185e5bff06359f14bf78b7d
purkayasta <pritompurkayasta@gmail.com>
2023-04-01 21:47:12 +0600

    added download service

--- src/BulkWallpaper.Service/DownloadService.cs
+++ src/BulkWallpaper.Service/DownloadService.cs
@@ -5,13 +5,26 @@
 		private readonly HttpClient _httpClient;
 		public DownloadService(HttpClient httpClient) => _httpClient = httpClient;
 
-		public async Task SaveAsync(HttpResponseMessage response, string localPathWithImageName)
+		public async Task DownloadAsync(string url, string localPath, string imageName, string imageExtension)
 		{
-			using var fileStream = await response.Content.ReadAsStreamAsync();
-			using var streamWriter = File.Open(localPathWithImageName, FileMode.Create);
-			await fileStream.CopyToAsync(streamWriter);
+			try
+			{
+				var physicalPathInfo = Directory.CreateDirectory(localPath);
+				if (!physicalPathInfo.Exists)
+					throw new Exception("Invalid Directory");
+
+				var downloadablePath = physicalPathInfo.FullName + "/" + imageName + "." + imageExtension;
+
+				using var stream = await _httpClient.GetStreamAsync(url);
+				using var fileStream = File.Create(downloadablePath);
+				await stream.CopyToAsync(fileStream);
+
+				await Console.Out.WriteLineAsync($"\n File downloaded at {downloadablePath} \n");
+			}
+			catch (Exception exception)
+			{
+				await Console.Out.WriteLineAsync(exception.Message);
+			}
 		}
-		public async Task<HttpResponseMessage> GetContentAsync(string url)
-			=> await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
 	}
 }
